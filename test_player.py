b3004f64b527d3b5b7f2f4a6cc3589c2ff7ce8a9
Luis <luis.here.j@gmail.com>
2021-04-05 17:55:06 +0200

    diapo 22

--- test_player.py
+++ test_player.py
@@ -1,3 +1,10 @@
 import unittest
 from player import Player
 
+
+
+
+class TestPlayer (unittest.TestCase):
+    def test_initialScore(self):
+        player = Player ()
+        self.assertEqual("0", player.getScoer())
